2e9521d726c5947daf81afe497377c6000a126f8
estherchinda <davidakachi9@gmail.com>
2025-07-07 16:54:43 +0100

    added events page layout

--- app/events/layout.tsx
+++ app/events/layout.tsx
@@ -8,22 +8,25 @@ export default function EventsLayout({
   children: React.ReactNode;
 }>) {
   return (
-     <div className="flex flex-col md:flex-row md:overflow-hidden min-h-screen">
-      {/* Sidebar wrapper */}
-      <div className="{`flex-none transition-all duration-300 ease-in-out w-64">
+    <div className="flex min-h-screen flex-col md:flex-row">
+      {/* Sidebar */}
+      <div className="w-64 flex-none transition-all duration-300 ease-in-out">
         <Sidebar />
       </div>
 
       {/* Main content */}
-      <div
-        className={`flex-grow p-6 md:overflow-y-auto`}
-      >
-        <div className="flex justify-end items-center gap-3 bg-white">
-            <NotificationsComponent />
-            <Avatar src="" name="esther chinda" />
+      <section className="flex-grow flex flex-col overflow-hidden">
+        {/* Top bar */}
+        <div className="flex justify-end items-center gap-3 bg-white px-6 py-3">
+          <NotificationsComponent />
+          <Avatar src="" name="esther chinda" />
         </div>
-        {children}
-      </div>
+
+        {/* Scrollable content */}
+        <div className="flex-grow overflow-y-auto p-6 bg-gray-50">
+          {children}
+        </div>
+      </section>
     </div>
   );
 }
